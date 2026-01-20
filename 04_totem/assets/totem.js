4ffdfd1b4277523b765c1edc8e45d8ffa08be629
Agathe Bonte <bonte.agathe@gmail.com>
2026-01-20 10:09:21 +0100

    totem

--- 04_totem/assets/totem.js
+++ 04_totem/assets/totem.js
@@ -1,5 +1,20 @@
-$('.js-slider').slick({
-    dots: true,
-    arrows: true,
-    slidesToShow: 1,
-})
+$('.js-slider').slick();
+
+
+const headers = document.querySelectorAll(".accordion-header");
+
+headers.forEach(header => {
+  header.addEventListener("click", () => {
+    const item = header.parentElement;
+
+    // Ferme les autres sections
+    document.querySelectorAll(".accordion-item").forEach(i => {
+      if (i !== item) {
+        i.classList.remove("active");
+      }
+    });
+
+    // Ouvre / ferme la section cliquée
+    item.classList.toggle("active");
+  });
+});
